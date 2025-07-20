2e35487ba6a7e9bb874ac7e75bf483092fd4e6e7
Krishna Ambati <krishnaambatiemail@gmail.com>
2025-07-20 13:53:14 -0700

    Message

--- JavaApplications/SecondClass.java
+++ JavaApplications/SecondClass.java
@@ -2,7 +2,17 @@ public class SecondClass {
 
 	public static void main(String args[]) {
 		// Printing text
-		System.out.println("Test456");
+		System.out.println(1+3);
+		System.out.println("Hello, World!");
+		System.out.println("The sum of 1 and 3 is: " + (1 + 3));
+		System.out.println("The product of 2 and 5 is: " + (2 * 5));
+		System.out.println("The difference of 10 and 4 is: " + (10 - 4));
+		System.out.println("The quotient of 20 and 4 is: " + (20 / 4));
+		System.out.println("The remainder of 10 and 3 is: " + (10 % 3));
+		System.out.println("The square of 6 is: " + (6 * 6));
+		System.out.println("The cube of 3 is: " + (3 * 3 * 3));
+		System.out.println("The square root of 16 is: " + Math.sqrt(16));
+		System.out.println("The absolute value of -5 is: " + Math.abs(-5));
 	}
 
 }
