c5cbc5a194153f15c8cb5f6ef4ddd4a7df25a8b9
Krishna Ambati <krishnaambatiemail@gmail.com>
2025-07-20 14:47:50 -0700

    Test

--- JavaApplications/FirstClass.java
+++ JavaApplications/FirstClass.java
@@ -4,6 +4,19 @@ public class FirstClass {
 	public static void main(String argsp[]) {
 		// Printing text
 		System.out.println("Test123");
+		int a;
+		int b;
+		int c;
+		a = 10;
+		b = 20;
+		c = a + b;
+		System.out.println("The value of c is: " + c);
+		System.out.println("The value of a is: " + a);
+		System.out.println("The value of b is: " + b);
+		System.out.println("The sum of a and b is: " + (a + b));
+		System.out.println("The sum of a and b is: " + c);
+		System.out.println("The sum of a and b is: " + (a + b));
+
 	}
 
 }
